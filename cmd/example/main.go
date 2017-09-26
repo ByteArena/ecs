45f5efd6621bf828c672d6736ae2b202a7a82643
Jérôme Schneider / Net Gusto <contact@netgusto.com>
2017-09-26 21:46:48 +0200

    Improved example.

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -6,31 +6,30 @@ import (
 	"github.com/bytearena/ecs"
 )
 
+// The Walk component; always design components as simple containers
+// without logic (except for getters/setters if useful)
 type Walk struct {
 	Direction string
 	Distance  float64
 }
 
+// The Walk component
 type Talk struct {
 	Message string
 }
 
 func main() {
 
+	// Initialize the ECS manager
 	manager := ecs.NewManager()
 
+	// Declare the components
 	walk := manager.NewComponent()
 	talk := manager.NewComponent()
 
-	manager.NewEntity().
-		AddComponent(walk, &Walk{
-			Direction: "north",
-			Distance:  12.4,
-		}).
-		AddComponent(talk, &Talk{
-			Message: "Fluctuat nec mergitur.",
-		})
-
+	// Create 3 entities, and provide their components
+	// Component data may be anything (interface{})
+	// Use pointers if you want to be able to mutate the data
 	manager.NewEntity().
 		AddComponent(walk, &Walk{
 			Direction: "east",
@@ -42,27 +41,39 @@ func main() {
 			Message: "Wassup?",
 		})
 
+	manager.NewEntity().
+		AddComponent(walk, &Walk{
+			Direction: "north",
+			Distance:  12.4,
+		}).
+		AddComponent(talk, &Talk{
+			Message: "Fluctuat nec mergitur.",
+		})
+
+	// Tags are masks that help identify entities that match the required components
 	walkers := ecs.BuildTag(walk)
 	talkers := ecs.BuildTag(talk)
 	walkertalkers := ecs.BuildTag(walkers, talkers)
 
-	// Walkers
-	fmt.Println("# All the walkers walk :")
+	// Process the walkers
+	fmt.Println("\n# All the walkers walk :")
 	for _, result := range manager.Query(walkers) {
 		walkAspect := result.Components[walk].(*Walk)
 		fmt.Println("I'm walking ", walkAspect.Distance, "km towards", walkAspect.Direction)
 	}
 
-	fmt.Println("")
-	fmt.Println("# All the talkers talk (and be mutated) :")
+	// Process the talkers
+	fmt.Println("\n# All the talkers talk (and be mutated) :")
 	for _, result := range manager.Query(talkers) {
 		talkAspect := result.Components[talk].(*Talk)
 		fmt.Println(talkAspect.Message, "Just sayin'.")
+
+		// Here we mutate the component for this entity
 		talkAspect.Message = "So I was like 'For real?' and he was like '" + talkAspect.Message + "'"
 	}
 
-	fmt.Println("")
-	fmt.Println("# All the talkers & walkers do their thing :")
+	// Process the talkers/walkers
+	fmt.Println("\n# All the talkers & walkers do their thing :")
 	for _, result := range manager.Query(walkertalkers) {
 		walkAspect := result.Components[walk].(*Walk)
 		talkAspect := result.Components[talk].(*Talk)
@@ -71,10 +82,11 @@ func main() {
 
 	///////////////////////////////////////////////////////////////////////////
 	// Demonstrating views
+	// To increase speed for repetitive queries, you can create cached views
+	// for entities matching a given tag
 	///////////////////////////////////////////////////////////////////////////
 
-	fmt.Println("")
-	fmt.Println("# Demonstrating views")
+	fmt.Println("\n# Demonstrating views")
 
 	talkersView := manager.CreateView(talkers)
 
@@ -83,8 +95,7 @@ func main() {
 			Message: "Ceci n'est pas une pipe",
 		})
 
-	fmt.Println("")
-	fmt.Println("# Should print 2 messages :")
+	fmt.Println("\n# Should print 2 messages :")
 	for _, result := range talkersView.Get() {
 		talkAspect := result.Components[talk].(*Talk)
 		fmt.Println(result.Entity.GetID(), "says", talkAspect.Message)
@@ -92,8 +103,7 @@ func main() {
 
 	manager.DisposeEntities(manager.Query(talkers).Entities()...)
 
-	fmt.Println("")
-	fmt.Println("# Talkers have been disposed; should not print any message below :")
+	fmt.Println("\n# Talkers have been disposed; should not print any message below :")
 	for _, result := range talkersView.Get() {
 		talkAspect := result.Components[talk].(*Talk)
 		fmt.Println(result.Entity.GetID(), "says", talkAspect.Message)
